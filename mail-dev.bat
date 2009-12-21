c065637a4994275630d1701aaf6ff6bdae6c54bc
Don Loritz <don@leftofdayton.net>
2009-12-20 21:51:32 -0500

    Updating mail-dev.bat

--- mail-dev.bat
+++ mail-dev.bat
@@ -1,31 +1,38 @@
-::@echo off
-
-
+@echo off
 
 set ns=main
+set srcpath=.\src\net\leftofdayton\mail
 
 call :setup 
 
+call textpad %srcpath%\main.clj
 
-:: main "jars":
+echo.
+echo.
+call git status
+echo.
+echo.
 
-set mycp=%cl%\clojure-1.0.0.jar
+:: main "jars"
+
+set mycp=clojure20091217.jar
+call :setcp .\src\net\leftofdayton\mail
 call :setcp %cl%\tlc2
 
 
 
-call :setcp %cl%\duck
-call :setcp %cl%\clojure-contrib.jar
+::call :setcp %cl%\duck
+::call :setcp %cl%\clojure-contrib.jar
 
 ::xnix:   call :setcp %cl%\jline-0.9.94.jar
-call :setcp %cl%\jetty-6.1.15.jar
-call :setcp %cl%\jetty-util-6.1.15.jar
+::call :setcp %cl%\jetty-6.1.15.jar
+::call :setcp %cl%\jetty-util-6.1.15.jar
 ::call :setcp %cl%\servlet-api-2.5-6.1.15.jar
-call :setcp %cl%\servlet-api-2.5-20081211.jar
+::call :setcp %cl%\servlet-api-2.5-20081211.jar
 
 
 :loop
-java -cp %mycp% clojure.main -e "(ns %ns%)(in-ns '%ns%)" -i "%ns%.clj" -r
+java -cp %mycp% clojure.main -e "(ns %ns%)(in-ns '%ns%)" -i "%srcpath%\%ns%.clj" -r
 ::pause
 goto loop
 
@@ -33,7 +40,7 @@ goto loop
 :setup
 set drive=%~d0
 set CLOJURE_DIR=%drive%\code\clojure
-set cl=%CLOJURE_DIR%\lib
+set cl=..\cljlib
 call :thisfol
 echo %tf%
 goto :eof
